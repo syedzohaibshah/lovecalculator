188bccdc5c97ad9f4d3a3d61e0efce4606ff0d05
syedzohaibshah <98155885+syedzohaibshah@users.noreply.github.com>
2023-10-06 06:49:32 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+ prompt("Enter the Ist name:");
+       prompt("Enter the 2nd name:");
+       
+       var n = Math.random()*100;
+       n= Math.floor(n)+1;
+       alert("The score of love is "+n+"%");
